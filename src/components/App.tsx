8230726570373adb2772b81e03ba4383e6b0e96c
kwokkan <kwokkan@users.noreply.github.com>
2020-09-20 16:49:25 +0100

    Enable tree shaking.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { IUser } from '../models';
 import { get } from '../services/randomUserService';
 
