67e9516a9748f1caee950495a16c9d600a6ce20b
kwokkan <kwokkan@users.noreply.github.com>
2020-09-20 22:29:30 +0100

    Add profile view.

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -6,11 +6,25 @@ interface IUserProps {
 }
 
 export function User(props: IUserProps) {
-    const { name } = props.user;
+    const { name, picture, location } = props.user;
+    const fullName = `${name.first} ${name.last}`;
 
     return (
         <div className="user">
-            <h3>{name.title} {name.first} {name.last}</h3>
+            <div className="user-header">
+                <img className="user-picture" src={picture.large} alt={fullName} title={fullName} width={256} height={256} />
+                <h3 className="user-name">{fullName}</h3>
+            </div>
+            <div className="user-content">
+                <address className="user-location">
+                    <i className="user-location-label">State</i>
+                    <strong>{location.state}</strong>
+                    <i className="user-location-label">City</i>
+                    <strong>{location.city}</strong>
+                    <i className="user-location-label">Street</i>
+                    <strong>{location.street.number} {location.street.name}</strong>
+                </address>
+            </div>
         </div>
     );
 }
