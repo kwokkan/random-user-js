0bcfbfd853841a546b50c32f7b6ac87f592fa0b4
kwokkan <kwokkan@users.noreply.github.com>
2020-09-20 16:37:05 +0100

    Fix Parcel async hack on build.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/App';
 
+// Parcel JS hack
+import 'regenerator-runtime/runtime';
+
 render(
     <App />,
     document.getElementById('app')
