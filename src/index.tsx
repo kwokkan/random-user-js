0ebf2e03e4805e5ee4d1857e591303489029bdfb
kwokkan <kwokkan@users.noreply.github.com>
2021-11-02 20:15:11 +0000

    Update to Parcel 2 to allow for newer Node versions to work.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import { h, render } from 'preact';
 import { App } from './components/App';
 
-// Parcel JS hack
-import 'regenerator-runtime/runtime';
-
 render(
     <App />,
     document.getElementById('app')!
