d59451323b2b37e9162e0908797fb3dc133def35
kwokkan <kwokkan@users.noreply.github.com>
2022-07-07 22:09:03 +0100

    Load styles locally instead of from unpkg.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
+import L from "leaflet";
 import { render } from 'preact';
 import { App } from './components/App';
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
 
 render(
     <App />,
